6186af46babd7ebba427ec652aa0afe86c141ef7
Anjali <holla2467@gmail.com>
2024-08-14 13:34:11 +0530

    FEAT: give visual feedback while adding or removing item from cart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,48 @@
 <script>
     import Product from "../components/Product.svelte";
     import books from "../data/books.js"
-    import { onMount } from "svelte";
+    import { fade } from 'svelte/transition';
+
+    let showSucess = false
+    let showFailure = false
+
+    function showCartSuccess(detail){
+        showSucess = true
+
+        setTimeout(()=>{
+            showSucess = false
+        }, 2000)
+    }
+
+    function showCartFailure(detail){
+        showFailure = true 
+
+        setTimeout(()=>{
+            showFailure = false
+        }, 2000)
+    }
 
     function itemAddHandler(event){
-        console.log("hello", event.detail.message)
+        showCartSuccess(event.detail)
     }
 
     function itemRemoveHandler(event){
-        console.log("hello", event.detail.message)
+        showCartFailure(event.detail)
     }
-
-    onMount(()=>{
-      
-    })
 
 </script>
 
-
+<div class="flex w-full relative items-center justify-center">
+    {#if showSucess}
+        <div class="p-3 rounded-xl bg-green-400  fixed  bottom-[20px]" transition:fade>
+            Item Added to Cart Successfully!
+        </div>
+    {:else if showFailure}
+        <div class="p-3 rounded-xl bg-red-400  fixed  bottom-[20px]" transition:fade>
+            Couldn't add item to the cart!
+        </div>
+    {/if}
+</div>
 
 <section class="flex overflow-hidden my-[50px] md:mx-[40px]  items-center justify-center md:justify-start ">
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-8">
@@ -27,9 +52,4 @@
     </div>
 </section>
 
-
-<!-- 
-<div class="p-3 rounded-xl bg-green-400  fixed right-[10px] top-[50px]">
-    Item Added to Cart Successfully!
-</div> -->
 
