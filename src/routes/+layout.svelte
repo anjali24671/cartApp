6740eebc1064c895e6333c6a7922b29a7e0bf220
Anjali <holla2467@gmail.com>
2024-08-16 13:45:12 +0530

    BUG: Position the cart icon status correctly

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
     import { onMount } from 'svelte';
     import cart from '../store/cartStore';
 
+
     // Userfront 
     import { PUBLIC_USERFRONT_ACCOUNT_ID } from '$env/static/public';
     import Userfront from '@userfront/toolkit/web-components';
@@ -60,13 +61,13 @@
         {/if}
         
         <!-- CART -->
-        <button class="text-gray-800 hover:text-black transition" on:click={() => goto('/cart')}>
-            <a href="/cart" class="relative">
-                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19" class="fill-current">
+        <button class="text-gray-800 hover:text-black transition relative" on:click={() => goto('/cart')}>
+            <a href="/cart" >
+                <svg class=" fill-current" xmlns="http://www.w3.org/2000/svg" width="23" height="19" viewBox="0 0 23 19" >
                     <path d="M8.094 14.263h11.568a.8.8 0 0 0 .567-.223.74.74 0 0 0 .241-.569.74.74 0 0 0-.241-.568.8.8 0 0 0-.567-.223H8.293a.85.85 0 0 1-.63-.243 1.13 1.13 0 0 1-.304-.64L5.763 1.188Q5.67.599 5.37.299 5.07 0 4.272 0H.83a.8.8 0 0 0-.577.243A.78.78 0 0 0 0 .822q0 .334.252.573t.577.238h3.244L5.627 11.99q.168 1.056.76 1.664.594.609 1.707.609m-1.785-3.47h13.405q1.124 0 1.717-.613.593-.615.76-1.68l.767-4.94.026-.202A1.4 1.4 0 0 0 23 3.175q0-.375-.268-.614-.267-.238-.73-.238H5.428l.021 1.572h15.673l-.661 4.443a1.16 1.16 0 0 1-.29.635q-.225.237-.624.238l-13.248.01zM8.881 19q.714 0 1.197-.472.482-.472.482-1.14 0-.67-.482-1.142-.483-.472-1.197-.472-.693 0-1.181.472a1.53 1.53 0 0 0-.488 1.141q0 .67.488 1.141.488.472 1.18.472m9.248 0q.693 0 1.181-.472t.488-1.14-.488-1.142a1.64 1.64 0 0 0-1.18-.472q-.694 0-1.192.472a1.5 1.5 0 0 0-.499 1.141q0 .67.499 1.141.499.472 1.191.472"></path>
                 </svg>
                 {#if cartCount > 0}
-                    <div class="absolute flex items-center justify-center rounded-full text-xs bg-red-600 text-white w-[15px] h-[15px] top-[9px] right-[-12px]">
+                    <div class="absolute flex items-center justify-center rounded-full text-xs bg-red-600 text-white w-[15px] h-[15px] top-[-10px] right-[-4px]">
                         {cartCount}
                     </div>
                 {/if}
